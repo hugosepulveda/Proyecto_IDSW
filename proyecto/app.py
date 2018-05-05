35b52d51e5326800bce02def744cfc3506db516e
hugosepulveda <hugo.sepulvedaa@sansano.usm.cl>
2018-05-05 14:51:05 -0300

    Form de login.

--- proyecto/app.py
+++ proyecto/app.py
@@ -1,32 +1,38 @@
 from flask import Flask, render_template, request
+from config import Config
+from forms import LoginForm
 from flask_login import login_user, login_required, LoginManager, logout_user
 #from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config.from_object(Config)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
-
-@app.route("/")
-def main():
-	render_template(url_for("index"))
 	
+@app.route("/")
 @app.route("/index")
-def pag_index():
-	return("hola c:")
+def index():
+	return(render_template('index.html', title = "Index"))
+
 	
 @app.route("/login", methods = ["GET", "POST"])
 def login():
-	"""if current_user.is_authenticated:
-		return redirect(url_for('index'))"""
-	#render_template("login.html", title = "Login")
-	pass
+	form_login = LoginForm()
+	return(render_template('login.html', title = "Login", form = form_login))
+
 
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect( url_for("login") )
-    
+    return(redirect( url_for("login") ))
+
+@app.route("/cliente")
+@login_required
+def cliente_interno():
+	return(render_template("cliente.html", title = "Cliente"))
+	
+
 if __name__ == "__main__":
 	app.run()
 	
