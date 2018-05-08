223dc2ed01b3a5051570ffbe32283e9ad65712aa
hugosep <hugo.sepulvedaa@sansano.usm.cl>
2018-05-07 23:51:16 -0300

    Funcionando.

--- proyecto/forms.py
+++ proyecto/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
