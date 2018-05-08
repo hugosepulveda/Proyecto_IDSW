d2b77105b7b09a5acd2cc845401fe181b6abaa4d
Sofía Carrasco Caimanque <sofi@MacBook-Air-de-Sofia.local>
2018-05-08 02:21:40 -0300

    agregar booststrap

--- proyecto/app.py
+++ proyecto/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from config import Config
 from forms import LoginForm
+from flask_bootstrap import Bootstrap
 from flask_login import login_user, login_required, LoginManager, logout_user
 #from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
@@ -8,13 +9,14 @@ app.config.from_object(Config)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
-	
+Bootstrap(app)
+
 @app.route("/")
 @app.route("/index")
 def index():
 	return(render_template('index.html', title = "Index"))
 
-	
+
 @app.route("/login", methods = ["GET", "POST"])
 def login():
 	form_login = LoginForm()
@@ -31,8 +33,7 @@ def logout():
 @login_required
 def cliente_interno():
 	return(render_template("cliente.html", title = "Cliente"))
-	
+
 
 if __name__ == "__main__":
 	app.run()
-	
