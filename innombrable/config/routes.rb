de607148a7b1430ffb764fb5bb4c6e10df99f84a
Sofía Carrasco Caimanque <sofi@MacBook-Air-de-Sofia.local>
2018-05-19 03:03:31 -0400

    update

--- innombrable/config/routes.rb
+++ innombrable/config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
     get 'login/login'
 
     get 'welcome/index'
-        
+
     resources :usuarios
     post 'usuarios/new' => 'usuarios#new'
     root 'welcome#index'
+    get 'cliente_interno/index'
+    post 'cliente_interno/new'=>'cliente_interno#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
