d197955d91100ee55fc3bc5c325e62301d7f1fa8
hugosep <hugo.sepulvedaa@sansano.usm.cl>
2018-05-21 19:32:30 -0400

    a

--- innombrable/app/controllers/sessions_controller.rb
+++ innombrable/app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
   	usuario = Usuario.find_by(rut: params[:session][:rut])
   	
-  	if usuario
+  	if usuario && usuario.contrasena == params[:session][:contrasena]
         log_in usuario
         redirect_to usuario
 
--- innombrable/test/controllers/cliente_interno_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ClienteInternoControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get cliente_interno_index_url
-    assert_response :success
-  end
-
-end
