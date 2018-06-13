0c2cdfec0e8f4565ead757f0168f569ae03cafd9
hugosep <hugo.sepulvedaa@sansano.usm.cl>
2018-06-13 18:45:47 -0400

    Se ve inventario según bodeguero

--- innombrable/app/controllers/bodegueros_controller.rb
+++ innombrable/app/controllers/bodegueros_controller.rb
@@ -6,9 +6,13 @@ class BodeguerosController < ApplicationController
   end
 
   def mod_solicitudes
+    
   end
 
   def mod_inventario
+    @bodeguero = Bodeguero.find_by(usuario_id: current_user.id)
+    #@bodega = Bodega.where(bodeguero_id: @bodeguero.id)
+    @materiales = Materiale.where(bodega_id: @bodeguero.bodega_id)
   end
   
 end
