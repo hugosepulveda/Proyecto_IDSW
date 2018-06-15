5468e067f30d04e862fff5d555e47c45b1b6a2ef
hugosep <hugo.sepulvedaa@sansano.usm.cl>
2018-06-14 20:51:44 -0400

    Hint frontend crear_solicitudes

--- innombrable/app/controllers/bodegueros_controller.rb
+++ innombrable/app/controllers/bodegueros_controller.rb
@@ -6,7 +6,7 @@ class BodeguerosController < ApplicationController
   end
 
   def mod_solicitudes
-    
+    @bodeguero = Bodeguero.find_by(usuario_id: current_user.id)
   end
 
   def mod_inventario
@@ -14,5 +14,6 @@ class BodeguerosController < ApplicationController
     #@bodega = Bodega.where(bodeguero_id: @bodeguero.id)
     @materiales = Materiale.where(bodega_id: @bodeguero.bodega_id)
   end
-  
+  def aprobar_sol
+  end
 end
