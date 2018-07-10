28866f7d16357a0cf801996a4b4c0d0eda401fa7
hugosep <hugo.sepulvedaa@sansano.usm.cl>
2018-07-09 23:45:21 -0400

    Agregado de Foundation javascript (exitoso)

--- innombrable/config/initializers/assets.rb
+++ innombrable/config/initializers/assets.rb
@@ -13,6 +13,8 @@ Rails.application.config.assets.precompile += %w( perfiles.css )
 Rails.application.config.assets.precompile += %w( crear_solicitudes.css )
 Rails.application.config.assets.precompile += %w( ver_solicitudes.css )
 Rails.application.config.assets.precompile += %w( bodegueros_tablas.css )
+Rails.application.config.assets.precompile += %w( foundation_m/js/vendor/jquery.js )
+Rails.application.config.assets.precompile += %w( foundation_m/js/vendor/foundation.js )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
